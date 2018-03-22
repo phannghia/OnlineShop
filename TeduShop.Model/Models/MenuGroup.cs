17c20ae2cf786c276e6c94d627aed33722af7dae
DESKTOP-TLM4OEA\Admin <thevisao1295@gmail.com>
2018-03-23 00:33:40 +0700

    Model

---
+++ TeduShop.Model/Models/MenuGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("MenuGroups")]
+  public   class MenuGroup
+    {
+        [Key]
+        public string ID { set; get; }
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
